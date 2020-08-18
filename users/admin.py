ade734f5a92f3e30e3c83dcd967b184abd35d7ff
wwan13 <wwan13@naver.com>
2020-08-18 18:06:54 +0900

    custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)  # admin.site.register(user)과 같은 의미
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     """Custom User Admin"""
 
-    list_display = ("username", "gender", "language", "currency", "superhost")
-    list_filter = ("language", "currency", "superhost")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "language",
+                    "birthdate",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+# 어우 개복잡해 미친건가
+# 튜플 안에 튜플 안에 리스트 안에 튜플 ?
