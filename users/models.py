cd4bccaba12fabb5050da548dc3cb5450897d7b6
wwan13 <wwan13@naver.com>
2020-08-18 17:24:01 +0900

    fix user model

--- users/models.py
+++ users/models.py
@@ -11,17 +11,41 @@ class User(AbstractUser):  # AbstractUser -> 장고의 기본 유저 모델
     # 상수 설정
 
     GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
+        (GENDER_MALE, "Male"),  # (DATABASE로 보내지는 값, ADMIN FORM에서 보여지는 값)
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
     # 선택지 (튜플)
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
 
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
     # null = True -> 비어있는거 허락 한다
     # default = "" -> string 값을 기본값으로 할당
     # blank = True -> db 생성시 꼭 값을 안넣어도 되게 함
