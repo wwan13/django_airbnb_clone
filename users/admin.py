56f771f168ec3f29205c15e47ecec8adf0951114
wwan13 <wwan13@naver.com>
2020-08-18 17:40:19 +0900

    fix admin.url

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.User)
+@admin.register(models.User)  # admin.site.register(user)과 같은 의미
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """Custom User Admin"""
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
