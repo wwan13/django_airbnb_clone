ea79dd5287e365ec84942b283a70e51a26a114aa
wwan13 <wwan13@naver.com>
2020-08-18 17:11:40 +0900

    Create User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):  # AbstractUser -> 장고의 기본 유저 모델
+    """custom user model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    # 상수 설정
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+    # 선택지 (튜플)
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    # null = True -> 비어있는거 허락 한다
+    # default = "" -> string 값을 기본값으로 할당
+    # blank = True -> db 생성시 꼭 값을 안넣어도 되게 함
