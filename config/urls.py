c8ec83cc3dfd597e9dcb81b70706089a45be231f
wwan13 <wwan13@naver.com>
2020-08-25 17:02:40 +0900

    fix typing mistake in #8.2

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEFIA_URL,document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
