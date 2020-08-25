2f5b6c5ff6ab086e3031850b217a454e2ba96bf5
wwan13 <wwan13@naver.com>
2020-08-25 19:16:56 +0900

    #9.2 seed_everything and seed_users

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,8 @@ PROJECT_APPS = [
     "conversations.apps.ConversationsConfig",
 ]
 
+THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
